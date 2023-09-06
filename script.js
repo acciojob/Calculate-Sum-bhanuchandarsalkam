0f390ac088b213d8ee1112b715aca22b600e103c
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-06 14:25:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 function get(){
 	const n1=document.getElementById("n1").value;
 	const n2=document.getElementById("n2").value;
-		document.getElementById("sum").innerText=n1+n2;
+	const a=parseInt(n1);
+	const b=parseInt(n2);
+		document.getElementById("sum").innerText=a+b;
 		//document.getElementById("sum").innerText="";
 }
 const sum=document.getElementById("sum_btn");
