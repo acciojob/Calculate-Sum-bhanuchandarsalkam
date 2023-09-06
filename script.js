708c7e973fc0c52b6aa4a39609245b644a6f3f86
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-06 14:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function get(){
+	const n1=document.getElementById("n1").value;
+	const n2=document.getElementById("n2").value;
+		document.getElementById("sum").innerText=n1+n2;
+		//document.getElementById("sum").innerText="";
+}
+const sum=document.getElementById("sum_btn");
+sum.addEventListener("click",get);
